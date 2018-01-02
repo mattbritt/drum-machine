5570c413e5b0c79823c2990302cbb611937c4dcd
Matt Britt <matthewlbritt@gmail.com>
2018-01-02 15:56:58 -0600

    disabled service worker

--- src/index.js
+++ src/index.js
@@ -2,10 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-import registerServiceWorker from './registerServiceWorker';
+//import registerServiceWorker from './registerServiceWorker';
 
 import {MainContainer} from './containers/MainContainer';
 //import {styles} from './helpers/styles';
 
 ReactDOM.render(<MainContainer />, document.getElementById('root'));
-registerServiceWorker();
+//registerServiceWorker();
